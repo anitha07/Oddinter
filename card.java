a14f5154f0bd3cc61c0fc5300270f49c3f06a2a3
anitha07 <anithasakthivel20@gmail.com>
2017-09-05 08:22:02 -0700

    Create card.java

---
+++ card.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class card
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    double a=2;
+    double b=2;
+    double c;
+    c=Math.pow(a,b);
+    System.out.println(c);
+    }
+    
+}
