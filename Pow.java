26719e0a8f16d514c706a7f0f66b0b13646d9242
anitha07 <anithasakthivel20@gmail.com>
2017-09-05 08:25:31 -0700

    Create Pow.java

---
+++ Pow.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Pow
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    double a=sc.nextDouble();
+    double b=sc.nextDouble();
+    double c;
+    c=Math.pow(a,b);
+    System.out.println(c);
+    }
+    
+}
